d9fa39b93dab02c356e9329c4738704cf0616092
andrealvesaraujo <andrealvesaraujo.1995@gmail.com>
2025-07-17 17:44:15 -0300

    feat: start project

---
+++ src/main/java/baeldung/simple_spring_boot_aplication/SimpleSpringBootAplicationApplication.java
@@ -0,0 +1,13 @@
+package baeldung.simple_spring_boot_aplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBootAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringBootAplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/baeldung/simple_spring_boot_aplication/SimpleSpringBootAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package baeldung.simple_spring_boot_aplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringBootAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
