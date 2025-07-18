259d9205214f7bc2982dba8857352f6602acbf51
andrealvesaraujo <andrealvesaraujo.1995@gmail.com>
2025-07-18 14:48:49 -0300

    fix: add getters and setters of book model

--- src/main/java/baeldung/simple_spring_boot_aplication/Book.java
+++ src/main/java/baeldung/simple_spring_boot_aplication/Book.java
@@ -5,6 +5,15 @@ import jakarta.persistence.*;
 @Entity
 public class Book {
 
+    public Book() {
+    }
+
+    public Book(long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
@@ -14,4 +23,28 @@ public class Book {
 
     @Column(nullable = false)
     private String author;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
