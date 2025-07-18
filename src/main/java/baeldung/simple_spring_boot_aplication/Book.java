eff58cc88a9a40eb7532573db5a2d611d8fe45df
andrealvesaraujo <andrealvesaraujo.1995@gmail.com>
2025-07-18 14:43:00 -0300

    feat: add simple persistence code about model and repo

---
+++ src/main/java/baeldung/simple_spring_boot_aplication/Book.java
@@ -0,0 +1,17 @@
+package baeldung.simple_spring_boot_aplication;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+}
